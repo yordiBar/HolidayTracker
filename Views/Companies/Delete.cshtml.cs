3684cead60d6971b998e56cefc318cb6804545a6
Bart <bart.rybak@hotmail.com>
2020-01-23 00:14:45 +0000

    new views

---
+++ Views/Companies/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HolidayTracker.Data;
+using HolidayTracker.Models.Company;
+
+namespace HolidayTracker.Views.Companies
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly HolidayTracker.Data.ApplicationDbContext _context;
+
+        public DeleteModel(HolidayTracker.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Company Company { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Company = await _context.Companies.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Company == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Company = await _context.Companies.FindAsync(id);
+
+            if (Company != null)
+            {
+                _context.Companies.Remove(Company);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
