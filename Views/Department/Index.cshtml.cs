249edfc37578ebb1601cb9dcde4636424677c1b1
Bart <bart.rybak@hotmail.com>
2020-02-12 23:44:20 +0000

    swf

--- Views/Department/Index.cshtml.cs
+++ Views/Department/Index.cshtml.cs
@@ -25,5 +25,49 @@ namespace HolidayTracker.Views.Departments
         public string CurrentSort { get; set; }
 
         public PaginatedList<Department> Department { get;set; }
-    }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
+        {
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            CodeSort = sortOrder == "Code" ? "code_desc" : "Code";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<Department> employeeIQ = from s in _context.Departments
+                                                select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                employeeIQ = employeeIQ.Where(s => s.DepartmentName.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    employeeIQ = employeeIQ.OrderByDescending(s => s.DepartmentName);
+                    break;
+                case "Date":
+                    employeeIQ = employeeIQ.OrderBy(s => s.DepartmentCode);
+                    break;
+                case "date_desc":
+                    employeeIQ = employeeIQ.OrderByDescending(s => s.DepartmentCode);
+                    break;
+                default:
+                    employeeIQ = employeeIQ.OrderBy(s => s.DepartmentName);
+                    break;
+            }
+
+            int pageSize = 3;
+            Department = await PaginatedList<Department>.CreateAsync(
+                employeeIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+        }
+    }        
 }
