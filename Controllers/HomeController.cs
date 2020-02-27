ad47497ee6d246df63ab4efb5ec8df386b66d72a
Bart <bart.rybak@hotmail.com>
2020-02-27 01:40:29 +0000

    dashboard view, datepicker, forms

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HolidayTracker.Models;
+using HolidayTracker.Models.RequestType;
 
 namespace HolidayTracker.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HolidayTracker.Data.ApplicationDbContext _context;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -38,6 +40,30 @@ namespace HolidayTracker.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult CreateRequest(string query)
+        {
+            List<RequestType> requestList = _context.RequestTypes.ToList();
+            List<RequestType> requestResults = new List<RequestType>();
+            foreach (var request in requestList)
+            {
+                if (request.RequestTypeName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    requestResults.Add(request);
+                }
+            }
+
+            requestResults.Sort(delegate (RequestType r1, RequestType r2) { return r1.RequestTypeName.CompareTo(r2.RequestTypeName); });
+
+            var serialisedJson = from result in requestResults
+                                 select new
+                                 {
+                                     name = result.RequestTypeName,
+                                     id = result.Id
+                                 };
+
+            return Json(serialisedJson);
         }
     }
 }
