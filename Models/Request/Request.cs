96378c8f52d34eb6918d231d51cf3c6960fa0366
Bart <bart.rybak@hotmail.com>
2020-03-01 14:13:15 +0000

    requests dahsboard created

--- Models/Request/Request.cs
+++ Models/Request/Request.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,9 +13,40 @@ namespace HolidayTracker.Models.Request
         public int RequestTypeId { get; set; }
         public int EmployeeId { get; set; }
         public int RequestCreatedByEmployeeId { get; set; }
-        public string From { get; set; }
-        public string To { get; set; }
+        public string From { get; set; } //change to DateTime
+        public string To { get; set; } //change to DateTime
         public string Status { get; set; }
         public double RequestAmount { get; set; }
+
+        //add description
     }
+
+    public class CreateRequestDTO
+    {
+        public int RequestTypeId { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Description { get; set; }
+
+        public DateTime RealFrom
+        {
+            get
+            {
+                return DateTime.ParseExact(From, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public DateTime RealTo
+        {
+            get
+            {
+                return DateTime.ParseExact(To, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            }
+        }
+    }
+
+    //public class MyRequestsViewModel
+    //{
+    //    public List<Request> AllRequests { get; set; }
+    //}
 }
