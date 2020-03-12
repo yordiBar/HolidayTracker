8158e1a210c636ea90429f1420489c708b4a6d56
Bart <bart.rybak@hotmail.com>
2020-03-12 01:25:53 +0000

    a few changes to startup and other

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,9 @@ using Microsoft.Extensions.Hosting;
 using HolidayTracker.Extensions;
 using Microsoft.Extensions.Options;
 using System.Security.Claims;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Routing;
 
 namespace HolidayTracker
 {
@@ -30,9 +33,36 @@ namespace HolidayTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>((Action<DbContextOptionsBuilder>)(options => options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"),
+                (Action<SqlServerDbContextOptionsBuilder>)null)), ServiceLifetime.Scoped, ServiceLifetime.Scoped);
+            services.AddDefaultIdentity<ApplicationUser>((Action<IdentityOptions>)(options => options.SignIn.RequireConfirmedAccount = false))
+                .AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, MyUserClaimsPrincipalFactory>();
+            MvcServiceCollectionExtensions.AddControllersWithViews(services);
+            MvcServiceCollectionExtensions.AddMvc(services);
+            services.Configure<IdentityOptions>((Action<IdentityOptions>)(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5.0);
+                options.Lockout.AllowedForNewUsers = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            }));
+            IdentityServiceCollectionExtensions.ConfigureApplicationCookie(services, (Action<CookieAuthenticationOptions>)(options =>
+           {
+               options.get_Cookie().set_HttpOnly(true);
+               options.set_ExpireTimeSpan(TimeSpan.FromMinutes(5.0));
+               options.set_LoginPath((PathString)"/Identity/Account/Login");
+               options.set_AccessDeniedPath((PathString)"/Identity/Account/AccessDenied");
+               options.set_SlidingExpiration(true);
+           }));
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
             //services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
             //    .AddRoles<IdentityRole>()
             //    .AddEntityFrameworkStores<ApplicationDbContext>()
@@ -40,146 +70,199 @@ namespace HolidayTracker
             //    //.AddDefaultTokenProviders();
 
 
-            services.AddIdentity<ApplicationUser, IdentityRole>(options => 
-            {
-                options.Password.RequiredLength = 6;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireDigit = true;
-            })
-                .AddRoleManager<RoleManager<IdentityRole>>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultUI()
-                .AddClaimsPrincipalFactory<MysUserClaimsPrincipalFactory>()
-                .AddDefaultTokenProviders();
+            //services.AddIdentity<ApplicationUser, IdentityRole>(options => 
+            //{
+            //    options.Password.RequiredLength = 6;
+            //    options.Password.RequireLowercase = false;
+            //    options.Password.RequireUppercase = false;
+            //    options.Password.RequireNonAlphanumeric = false;
+            //    options.Password.RequireDigit = true;
+            //})
+            //    .AddRoleManager<RoleManager<IdentityRole>>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>()
+            //    .AddDefaultUI()
+            //    .AddClaimsPrincipalFactory<MyUserClaimsPrincipalFactory>()
+            //    .AddDefaultTokenProviders();
 
-            services.AddControllersWithViews();
-            services.AddRazorPages();
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            //services.AddControllersWithViews();
+            //services.AddRazorPages();
+            //services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
-            if (env.IsDevelopment())
+            if (HostEnvironmentEnvExtensions.IsDevelopment((IHostEnvironment)env))
             {
-                app.UseDeveloperExceptionPage();
+                DeveloperExceptionPageExtensions.UseDeveloperExceptionPage(app);
                 app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                ExceptionHandlerExtensions.UseExceptionHandler(app, "/Home/Error");
+                HstsBuilderExtensions.UseHsts(app);
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-            //app.UseMvc(routes =>
-            //{
-            //    routes.MapRoute(
-            //        name: "default",
-            //        template: "{controller=Home}/{action=Index}/{id?}");
-            //});
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
-            try
-            {
-                CreateRoles(services).Wait();
-            }
-            catch(Exception ae)
-            {
-                throw ae;
-            }
-            
+            HttpsPolicyBuilderExtensions.UseHttpsRedirection(app);
+            StaticFileExtensions.UseStaticFiles(app);
+            EndpointRoutingApplicationBuilderExtensions.UseRouting(app);
+            AuthAppBuilderExtensions.UseAuthentication(app);
+            AuthorizationAppBuilderExtensions.UseAuthorization(app);
+            EndpointRoutingApplicationBuilderExtensions.UseEndpoints(app, (Action<IEndpointRouteBuilder>)(endpoints =>
+        {
+            ControllerEndpointRouteBuilderExtensions.MapControllerRoute(endpoints, "default", "{controller=Home}/{action=Index}/{id?}", (object)null, (object)null, (object)null);
+            RazorPagesEndpointRouteBuilderExtensions.MapRazorPages(endpoints);
+        }));
+            this.CreateRoles(services).Wait();
         }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
+        //{
+        //    if (env.IsDevelopment())
+        //    {
+        //        app.UseDeveloperExceptionPage();
+        //        app.UseDatabaseErrorPage();
+        //    }
+        //    else
+        //    {
+        //        app.UseExceptionHandler("/Home/Error");
+        //        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        //        app.UseHsts();
+        //    }
+        //    app.UseHttpsRedirection();
+        //    app.UseStaticFiles();
+
+        //    app.UseRouting();
+
+        //    app.UseAuthentication();
+        //    app.UseAuthorization();
+        //    //app.UseMvc(routes =>
+        //    //{
+        //    //    routes.MapRoute(
+        //    //        name: "default",
+        //    //        template: "{controller=Home}/{action=Index}/{id?}");
+        //    //});
+
+        //    app.UseEndpoints(endpoints =>
+        //    {
+        //        endpoints.MapControllerRoute(
+        //            name: "default",
+        //            pattern: "{controller=Home}/{action=Index}/{id?}");
+        //        endpoints.MapRazorPages();
+        //    });
+        //    try
+        //    {
+        //        CreateRoles(services).Wait();
+        //    }
+        //    catch(Exception ae)
+        //    {
+        //        throw ae;
+        //    }
+
+        //}
 
         private async Task CreateRoles(IServiceProvider serviceProvider)
         {
-            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-
-            IdentityResult roleResult;
-            //here in this line we are adding Admin Role
-
-            var systemAdmin = await RoleManager.RoleExistsAsync("SystemAdmin");
-            if (!systemAdmin)
+            RoleManager<IdentityRole> RoleManager = ServiceProviderServiceExtensions.GetRequiredService<RoleManager<IdentityRole>>(serviceProvider);
+            UserManager<ApplicationUser> UserManager = ServiceProviderServiceExtensions.GetRequiredService<UserManager<ApplicationUser>>(serviceProvider);
+            bool roleCheck = await RoleManager.RoleExistsAsync("Admin");
+            if (!roleCheck)
             {
-                //here in this line we are creating SystemAdmin role and seed it to the database
-                roleResult = await RoleManager.CreateAsync(new IdentityRole("SystemAdmin"));
+                IdentityResult roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
             }
-
-            var adminRole = await RoleManager.RoleExistsAsync("Admin");
-            if (!adminRole)
-            {
-                //here in this line we are creating admin role and seed it to the database
-                roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
-            }
-
-            //here we are assigning the Admin role to the User that we have registered above 
-            //Now, we are assigning admin role to this user("admin@admin.com"). When will we run this project then it will
-            //be assigned to that user.
-            ApplicationUser user = await UserManager.FindByEmailAsync("admin@admin.com");
-            if(user == null)
-            {
-                var newuser = new ApplicationUser { UserName = "admin@admin.com", Email = "admin@admin.com" };
-                var result = await UserManager.CreateAsync(newuser, "Admin0!");
-                user = await UserManager.FindByEmailAsync("admin@admin.com");
-            }
-            
-            await UserManager.AddToRoleAsync(user, "SystemAdmin");
-
-            var managerRole = await RoleManager.RoleExistsAsync("Manager");
-            if (!managerRole)
-            {
-                //here in this line we are creating Manager role and seed it to the database
-                roleResult = await RoleManager.CreateAsync(new IdentityRole("Manager"));
-            }
-
-            var approverRole = await RoleManager.RoleExistsAsync("Approver");
-            if (!approverRole)
-            {
-                //here in this line we are creating Approver role and seed it to the database
-                roleResult = await RoleManager.CreateAsync(new IdentityRole("Approver"));
-            }
-
-            var employeeRole = await RoleManager.RoleExistsAsync("Employee");
-            if (!employeeRole)
-            {
-                //here in this line we are creating Employee role and seed it to the database
-                roleResult = await RoleManager.CreateAsync(new IdentityRole("Employee"));
-            }
+            ApplicationUser user = await UserManager.FindByEmailAsync("bart.rybak@hotmail.com");
+            bool flag = await UserManager.IsInRoleAsync(user, "Admin");
+            if (flag)
+                return;
+            IdentityResult roleAsync = await UserManager.AddToRoleAsync(user, "Admin");
         }
 
+        //    private async Task CreateRoles(IServiceProvider serviceProvider)
+        //    {
+        //        var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        //        var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-    }
+        //        IdentityResult roleResult;
+        //        //here in this line we are adding Admin Role
 
-    public class MysUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>
-    {
-        public MysUserClaimsPrincipalFactory(UserManager<ApplicationUser> userManager,
-            RoleManager<IdentityRole> roleManager,
-            IOptions<IdentityOptions> optionsAssessor)
-            : base(userManager, roleManager, optionsAssessor)
+        //        var systemAdmin = await RoleManager.RoleExistsAsync("SystemAdmin");
+        //        if (!systemAdmin)
+        //        {
+        //            //here in this line we are creating SystemAdmin role and seed it to the database
+        //            roleResult = await RoleManager.CreateAsync(new IdentityRole("SystemAdmin"));
+        //        }
+
+        //        var adminRole = await RoleManager.RoleExistsAsync("Admin");
+        //        if (!adminRole)
+        //        {
+        //            //here in this line we are creating admin role and seed it to the database
+        //            roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
+        //        }
+
+        //        //here we are assigning the Admin role to the User that we have registered above 
+        //        //Now, we are assigning admin role to this user("admin@admin.com"). When will we run this project then it will
+        //        //be assigned to that user.
+        //        ApplicationUser user = await UserManager.FindByEmailAsync("admin@admin.com");
+        //        if(user == null)
+        //        {
+        //            var newuser = new ApplicationUser { UserName = "admin@admin.com", Email = "admin@admin.com" };
+        //            var result = await UserManager.CreateAsync(newuser, "Admin0!");
+        //            user = await UserManager.FindByEmailAsync("admin@admin.com");
+        //        }
+
+        //        await UserManager.AddToRoleAsync(user, "SystemAdmin");
+
+        //        var managerRole = await RoleManager.RoleExistsAsync("Manager");
+        //        if (!managerRole)
+        //        {
+        //            //here in this line we are creating Manager role and seed it to the database
+        //            roleResult = await RoleManager.CreateAsync(new IdentityRole("Manager"));
+        //        }
+
+        //        var approverRole = await RoleManager.RoleExistsAsync("Approver");
+        //        if (!approverRole)
+        //        {
+        //            //here in this line we are creating Approver role and seed it to the database
+        //            roleResult = await RoleManager.CreateAsync(new IdentityRole("Approver"));
+        //        }
+
+        //        var employeeRole = await RoleManager.RoleExistsAsync("Employee");
+        //        if (!employeeRole)
+        //        {
+        //            //here in this line we are creating Employee role and seed it to the database
+        //            roleResult = await RoleManager.CreateAsync(new IdentityRole("Employee"));
+        //        }
+        //    }
+
+
+        //}
+
+        public class MyUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>
         {
-        }
+            public MyUserClaimsPrincipalFactory(UserManager<ApplicationUser> userManager,
+                RoleManager<IdentityRole> roleManager,
+                IOptions<IdentityOptions> optionsAssessor)
+                : base(userManager, roleManager, optionsAssessor)
+            {
+            }
 
-        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
-        {
-            var identity = await base.GenerateClaimsAsync(user);
-            identity.AddClaim(new Claim("CompanyId", user.CompanyId.ToString()));
-            return identity;
+            public override async Task<ClaimsPrincipal> CreateAsync(
+                ApplicationUser user)
+            {
+                ClaimsPrincipal principal = await base.CreateAsync(user);
+                ((ClaimsIdentity)principal.Identity).AddClaims((IEnumerable<Claim>)new Claim[1]
+            {
+                new Claim("CompanyId", user.CompanyId.ToString())
+            });
+                return principal;
+            }
+
+            //protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+            //{
+            //    var identity = await base.GenerateClaimsAsync(user);
+            //    identity.AddClaim(new Claim("CompanyId", user.CompanyId.ToString()));
+            //    return identity;
+            //}
+
         }
-        
     }
 }
