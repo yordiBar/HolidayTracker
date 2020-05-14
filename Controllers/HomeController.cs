11105bc960d1effeb8dca44241587dad8b0324e2
Bart <bart.rybak@hotmail.com>
2020-05-14 21:35:37 +0100

    about page added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -95,6 +95,12 @@ namespace HolidayTracker.Controllers
             return View();
         }
 
+        // Action method to display About page
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
